75760bf77105c95f396e72e0a9ecd60dcb219447
Rodrigo Viana de Oliveira <roviol@gmail.com>
2020-06-19 23:00:53 -0300

    Route 'likes' improved

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const {uuid, isUuid} = require("uuidv4");
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
 const repositories = [];
 
 function validateId (request, response, next) {
@@ -14,12 +17,10 @@ function validateId (request, response, next) {
     return response.status(400).json({error: 'Invalid repository ID.'});
   }
 
-  return next();
+  next();
 
 }
 
-app.use(express.json());
-app.use(cors());
 
 // 
 // R O T A S
@@ -93,26 +94,17 @@ app.post("/repositories/:id/like", validateId, (request, response) => {
 
   const repositoryIndex = repositories.findIndex(repository => repository.id == id);
 
-  if (repositoryIndex < 0) {
+  const repository = repositories.find(repository => repository.id == id);
+
+  if(!repository) {
     return response.status(400).json({error: 'Repository not found!'});
   }
 
-  const title = repositories[repositoryIndex].title;
-  const url = repositories[repositoryIndex].url;
-  const techs = repositories[repositoryIndex].techs;
-  const likes = repositories[repositoryIndex].likes + 1;
-      
-  const repository = {
-    id,
-    title,
-    url,
-    techs,
-    likes
-  }
+  repository.likes = repository.likes + 1;
 
   repositories[repositoryIndex] = repository;
 
-  return response.status(204);
+  return response.status(204).send();
 
 
 });
